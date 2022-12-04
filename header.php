f810291d96843c918cd3e2418776e2568a026a98
Melin <melinfebriani19@gmail.com>
2022-12-04 19:57:00 +0700

    update header

--- header.php
+++ header.php
@@ -39,7 +39,7 @@ if(isset($message)){
          <div class="icons">
             <div id="menu-btn" class="fas fa-bars"></div>
             <a href="search_page.php" class="fas fa-search"></a>
-            <div id="user-btn" class="fas fa-user"></div>
+            <a href="profile.php" class="fas fa-user"></a>
             <?php
                $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
                $cart_rows_number = mysqli_num_rows($select_cart_number); 
